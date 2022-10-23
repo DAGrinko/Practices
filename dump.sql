069d559785bf154e1257e15d185e34a8e951e169
DAGrinko <115646315+DAGrinko@users.noreply.github.com>
2022-10-23 14:41:03 +0700

    Add files via upload

--- dump.sql
+++ dump.sql
@@ -15,26 +15,6 @@ CREATE SCHEMA IF NOT EXISTS `Clothing_Store` DEFAULT CHARACTER SET utf8 ;
 USE `Clothing_Store` ;
 
 -- -----------------------------------------------------
--- Table `Clothing_Store`.`Product`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Clothing_Store`.`Product` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `price_without_discount` DECIMAL(10,2) NOT NULL,
-  `price_with_promocode` DECIMAL(10,2) NOT NULL,
-  `discription` TINYTEXT NULL,
-  `status` VARCHAR(45) NOT NULL,
-  `main_img` VARCHAR(45) NOT NULL,
-  `main_section` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `Id_UNIQUE` (`id` ASC) VISIBLE,
-  UNIQUE INDEX `main_img_UNIQUE` (`main_img` ASC) VISIBLE,
-  UNIQUE INDEX `main_section_UNIQUE` (`main_section` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `Clothing_Store`.`Section`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Clothing_Store`.`Section` (
@@ -55,6 +35,37 @@ CREATE TABLE IF NOT EXISTS `Clothing_Store`.`Image` (
   `alt` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Clothing_Store`.`Product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Clothing_Store`.`Product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `price_without_discount` DECIMAL(10,2) NOT NULL,
+  `price_with_promocode` DECIMAL(10,2) NOT NULL,
+  `discription` TINYTEXT NULL,
+  `status` INT NOT NULL,
+  `main_img` INT NOT NULL,
+  `main_section` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `Id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `main_img_UNIQUE` (`main_img` ASC) VISIBLE,
+  UNIQUE INDEX `main_section_UNIQUE` (`main_section` ASC) VISIBLE,
+  INDEX `main_section_product_idx` (`id` ASC, `main_section` ASC) VISIBLE,
+  CONSTRAINT `main_section_product`
+    FOREIGN KEY (`id` , `main_section`)
+    REFERENCES `Clothing_Store`.`Section` (`id` , `id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `main_img_product`
+    FOREIGN KEY (`main_img`)
+    REFERENCES `Clothing_Store`.`Image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
